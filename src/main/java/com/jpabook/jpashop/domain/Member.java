023ad8f34b07e575897ca5f4d6853348ca5bfa22
kdo_1999/mac <kdo_1999@naver.com>
2023-08-30 19:54:05 +0900

    회원등록, 수정, 조회 API 개발 기본 학습

--- src/main/java/com/jpabook/jpashop/domain/Member.java
+++ src/main/java/com/jpabook/jpashop/domain/Member.java
@@ -1,25 +1,29 @@
 package com.jpabook.jpashop.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
