a170f90e500f38ed1efbc5a4d0c7099be4dde47f
kdo_1999/mac <kdo_1999@naver.com>
2023-09-01 18:29:33 +0900

    컬렉션 조회 최적화 학습

--- src/main/java/com/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/com/jpabook/jpashop/repository/OrderRepository.java
@@ -85,6 +85,26 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    public List<Order> findAllWithItem() {
+        //스프링3부터는 하이버네이트6을 사용하면서 distinct를 사용하지않아도 자동으로 중복제거 해준다.
+        return em.createQuery("select distinct o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d" +
+                " join fetch o.orderItems oi" +
+                " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery("" +
+                "select o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
 
     /**
      * JPA Criteria
